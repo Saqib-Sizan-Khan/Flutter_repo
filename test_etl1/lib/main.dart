dcdb0fd2adb875bc776417dd507a3cd9bed263fb
Saqib Sizan Khan <ssk123098@gmail.com>
2023-05-23 22:11:25 +0600

    whatsapp status page update

--- test_etl1/lib/main.dart
+++ test_etl1/lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Messenger_Chats_UI(),
+      home: WP_UI(),
     );
   }
 }
--- test_etl1/lib/wp_UI.dart
+++ test_etl1/lib/wp_UI.dart
@@ -173,45 +173,80 @@ class WP_UI extends StatelessWidget {
 
             //Status Page
             Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Container(
-                  child: ListView.builder(
-                    itemCount: 1,
-                      itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text('My Status'),
-                    );
-                  }),
-                ),
-                Container(
-                  child: ListView.builder(
-                    itemCount: arText.length,
-                      itemBuilder: (context, index) {
-                    return ListTile(
+                    padding: EdgeInsets.only(top: 10),
+                    width: 400,
+                    height: 70,
+                    child: ListTile(
                       title: Text(
-                        arText[index],
-                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                        'My Status',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
                       ),
-                      subtitle: Row(
+                      subtitle: Text(
+                        'Tap to add status update',
+                        style: TextStyle(fontSize: 14),
+                      ),
+                      leading: Stack(
                         children: [
-                          Text(
-                            'Yesterday, 9:19 am',
-                            style: TextStyle(
-                              fontSize: 14,
+                          CircleAvatar(
+                            backgroundImage: AssetImage('assets/images/53.jpg'),
+                            radius: 25,
+                          ),
+                          Positioned(
+                            bottom: 1,
+                            right: 1,
+                            child: CircleAvatar(
+                              backgroundColor: Colors.teal,
+                              radius: 10,
+                              child: Icon(
+                                Icons.add,
+                                size: 16,
+                                color: Colors.white,
+                              ),
                             ),
                           ),
                         ],
                       ),
-                      leading: CircleAvatar(
-                        radius: 25,
-                        backgroundColor: Colors.green,
-                        child: CircleAvatar(
-                          radius: 20,
-                          backgroundImage: arImage[index],
-                        ),
-                      ),
-                    );
-                  }),
+                    )),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Text('Recent updates', style: TextStyle(fontSize: 14)),
+                ),
+                Container(
+                  width: 400,
+                  height: 690,
+                  child: ListView.builder(
+                      itemCount: arText.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(
+                            arText[index],
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          subtitle: Row(
+                            children: [
+                              Text(
+                                'Yesterday, 9:19 am',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                          leading: CircleAvatar(
+                            radius: 25,
+                            backgroundColor: Colors.green,
+                            child: CircleAvatar(
+                              radius: 20,
+                              backgroundImage: arImage[index],
+                            ),
+                          ),
+                        );
+                      }),
                 ),
               ],
             ),
