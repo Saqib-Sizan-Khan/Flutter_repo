d106730acba428193357b27b33fbb225a010d20a
Saqib Sizan Khan <ssk123098@gmail.com>
2023-06-04 10:29:54 +0600

    Real Estated App UI update

--- test_etl1/lib/Real_Estated_App/rea_home_page.dart
+++ test_etl1/lib/Real_Estated_App/rea_home_page.dart
@@ -8,37 +8,106 @@ class REA_Home_Page extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 100,
-        backgroundColor: Colors.transparent,
+        backgroundColor: Colors.white,
         elevation: 0.0,
         leadingWidth: 100,
-        leading: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              primary: Colors.white,
-              side: BorderSide(width: 1, color: Colors.grey),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20)
-              ),
-            ),
-            onPressed: null,
-            child: Icon(Icons.menu_sharp, color: Colors.black),
+        leading: Container(
+          width: 50,
+          height: 50,
+          margin: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(width: 1, color: Colors.grey),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: ImageIcon(
+            AssetImage('assets/images/icons/menu.png'),
+            color: Colors.black,
           ),
         ),
-
         actions: [
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.white,
-                side: BorderSide(width: 1, color: Colors.grey),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)
+          Container(
+            width: 60,
+            height: 60,
+            margin: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(width: 1, color: Colors.grey),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: ImageIcon(
+              AssetImage('assets/images/icons/settings.png'),
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              flex: 1,
+              child: Container(
+                width: double.maxFinite,
+                margin: EdgeInsets.only(left: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'City',
+                      style: TextStyle(fontSize: 25, color: Colors.grey),
+                    ),
+                    Text(
+                      'San Francisco',
+                      style: TextStyle(
+                          fontSize: 50,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ],
                 ),
-              ),
-              onPressed: null,
-              child: Icon(Icons.equalizer, color: Colors.black),
+              )),
+          Expanded(
+              flex: 1,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Divider(
+                      color: Colors.grey,
+                      thickness: 1,
+                    ),
+                  ),
+                  Container(
+                    width: double.maxFinite,
+                    height: 80,
+                    child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 5,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            width: 150,
+                            margin: EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 10),
+                            decoration: BoxDecoration(
+                                color: Colors.grey[300],
+                                borderRadius: BorderRadius.circular(40)),
+                            child: Center(
+                                child: Text(
+                              'For sale',
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            )),
+                          );
+                        }),
+                  )
+                ],
+              )),
+
+          
+          Expanded(
+            flex: 6,
+            child: Container(
+              color: Colors.blue,
             ),
           ),
         ],
