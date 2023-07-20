ba98d0ab05340b887ff404ce075b140f2d37f3a2
Saqib Sizan Khan <ssk123098@gmail.com>
2023-07-20 21:39:46 +0600

    Weather App UI update

--- dart_core/lib/weather_app/weather_ui2.dart
+++ dart_core/lib/weather_app/weather_ui2.dart
@@ -7,35 +7,27 @@ class WeatherUI2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF4A98FA),
-      body: Column(
-        children: [
-          Expanded(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Wrap(
-                      verticalDirection: VerticalDirection.up,
-                      children: [
-                        Image.asset('assets/images/left_arrow_logo.png',
-                            scale: 1.5),
-                        SizedBox(width: 10),
-                        Text(
-                          'Back',
-                          style: TextStyle(fontSize: 20, color: Colors.white),
-                        ),
-                      ],
-                    ),
-                    Image.asset('assets/images/settings_logo.png', scale: 1.5),
-                  ],
-                ),
-              )),
-          Expanded(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
+      appBar: AppBar(
+        toolbarHeight: 80,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Row(children: [
+          Image.asset('assets/images/left_arrow_logo.png', scale: 1.5),
+          SizedBox(width: 10),
+          Text('Back', style: TextStyle(fontSize: 22, color: Colors.white))
+        ]),
+        actions: [
+          Image.asset('assets/images/settings_logo.png', scale: 1.5),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 16, left: 8, right: 8),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -50,86 +42,79 @@ class WeatherUI2 extends StatelessWidget {
                         style: TextStyle(fontSize: 24, color: Colors.white))
                   ],
                 ),
-              )),
-          Expanded(
-              flex: 2,
-              child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
+              ),
+              Container(
+                height: 200,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemCount: 10,
+                    itemBuilder: (context, index) => Container(
+                          margin: EdgeInsets.symmetric(horizontal: 5),
+                          width: 100,
+                          decoration: BoxDecoration(
+                              color: Colors.white.withOpacity(0.2),
+                              borderRadius: BorderRadius.circular(20)),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text('22°C',
+                                  style: TextStyle(
+                                      fontSize: 22, color: Colors.white)),
+                              Image.asset('assets/images/img_1.png'),
+                              Text('16.00',
+                                  style: TextStyle(
+                                      fontSize: 22, color: Colors.white)),
+                            ],
+                          ),
+                        )),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 24, horizontal: 8),
+                child: Text(
+                  'Next Forecast',
+                  style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white),
+                ),
+              ),
+              ListView.builder(
                   itemCount: 10,
-                  itemBuilder: (context, index) => Container(
-                        margin: EdgeInsets.symmetric(horizontal: 5),
-                        width: 100,
-                        decoration: BoxDecoration(
-                            color: Colors.white.withOpacity(0.2),
-                            borderRadius: BorderRadius.circular(20)),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) => Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Text('22°C',
+                            Text('Sep, 13',
                                 style: TextStyle(
                                     fontSize: 22, color: Colors.white)),
-                            Image.asset('assets/images/img_1.png'),
-                            Text('16.00',
+                            Image.asset('assets/images/img_1.png', scale: 1.6),
+                            Text('21°C',
                                 style: TextStyle(
                                     fontSize: 22, color: Colors.white)),
                           ],
                         ),
-                      ))),
-          Expanded(
-              flex: 5,
-              child: Container(
-                margin: EdgeInsets.only(top: 30, left: 20, right: 40),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Next Forecast',
+                      )),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.light_mode, size: 30, color: Colors.white),
+                  SizedBox(width: 10),
+                  Text('AccuWeather',
                       style: TextStyle(
-                          fontSize: 28,
+                          fontSize: 24,
                           fontWeight: FontWeight.w600,
-                          color: Colors.white),
-                    ),
-                    SizedBox(height: 20),
-                    Container(
-                      height: 350,
-                      child: ListView.builder(
-                          itemCount: 10,
-                          itemBuilder: (context, index) => Padding(
-                                padding: const EdgeInsets.all(12.0),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text('Sep, 13',
-                                        style: TextStyle(
-                                            fontSize: 22, color: Colors.white)),
-                                    Image.asset('assets/images/img_1.png',
-                                        scale: 1.6),
-                                    Text('21°C',
-                                        style: TextStyle(
-                                            fontSize: 22, color: Colors.white)),
-                                  ],
-                                ),
-                              )),
-                    ),
-                    SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.light_mode, size: 30, color: Colors.white),
-                        SizedBox(width: 10),
-                        Text('AccuWeather',
-                            style: TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.white)),
-                      ],
-                    )
-                  ],
-                ),
-              )),
-        ],
+                          color: Colors.white)),
+                ],
+              ),
+              SizedBox(height: 10)
+            ],
+          ),
+        ),
       ),
     );
   }
